2fc0322551ef44ed06a9b2f85f4e3da30860f2a0
curtis2 <18292967668@163.com>
2017-11-02 18:14:02 +0800

    first commit

---
+++ Collection/src/main/java/com/simon/collection/collection/Iterator.java
@@ -0,0 +1,24 @@
+package com.simon.collection.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Iterator {
+    public static void main(String[] args){
+        Collection collection=new ArrayList();
+        collection.add("A");
+        collection.add("B");
+        collection.add("C");
+
+        java.util.Iterator iterator = collection.iterator();
+        while (iterator.hasNext()){
+            String str = (String) iterator.next();
+            if(str.equals("A")){
+                //throw java.util.ConcurrentModificationException
+                iterator.remove();
+            }
+            System.out.println(str);
+        }
+        System.out.println(collection);
+    }
+}
